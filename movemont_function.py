8d570f70d8455653538fd176312ab665049d56ac
microStationCorp <sujanmondal916@gmail.com>
2020-04-03 00:02:11 +0530

    rubik cube solving started

---
+++ movemont_function.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+# front=red,index 1
+# left=green,index 0
+# right=blue, index 2
+# up=white, index 4
+# down=yellow, index 5
+# back=orange, index 3
+
+# rotate face clockwise or anti-clockwise
+def face_rotate(cube_side, clockwise=True):
+    if clockwise:
+        temp = cube_side[0, :].copy()
+        cube_side[0, :] = np.flip(cube_side[:, 0])
+        cube_side[:, 0] = cube_side[2, :]
+        cube_side[2, :] = np.flip(cube_side[:, 2])
+        cube_side[:, 2] = temp
+    else:
+        temp = cube_side[0, :].copy()
+        cube_side[0, :] = cube_side[:, 2]
+        cube_side[:, 2] = np.flip(cube_side[2, :])
+        cube_side[2, :] = cube_side[:, 0]
+        cube_side[:, 0] = np.flip(temp)
+    return cube_side
+
+
+def move_left_a(cube):
+    # set green face
+    cube[0] = face_rotate(cube[0].copy(), False)
+    # set red
+    temp = cube[1][:, 0].copy()
+    cube[1][:, 0] = cube[5][:, 0].copy()
+    cube[5][:, 0] = cube[3][:, 2].copy()
+    cube[3][:, 2] = cube[4][:, 0].copy()
+    cube[4][:, 0] = temp
+    return cube
+
+
+def move_left_c(cube):
+    # set green face
+    cube[0] = face_rotate(cube[0].copy())
+    return cube
---
+++ test_1_list_making.py
@@ -0,0 +1,49 @@
+import numpy as np
+# from . import movemont_function as mf
+from movemont_function import *
+
+END = np.array([
+    [
+        ['g1', 'g2', 'g3'],
+        ['g4', 'g5', 'g6'],
+        ['g7', 'g8', 'g9'],
+    ], [
+        ['r1', 'r2', 'r3'],
+        ['r4', 'r5', 'r6'],
+        ['r7', 'r8', 'r9'],
+    ], [
+        ['b1', 'b2', 'b3'],
+        ['b4', 'b5', 'b6'],
+        ['b7', 'b8', 'b9'],
+    ], [
+        ['o1', 'o2', 'o3'],
+        ['o4', 'o5', 'o6'],
+        ['o7', 'o8', 'o9'],
+    ], [
+        ['w1', 'w2', 'w3'],
+        ['w4', 'w5', 'w6'],
+        ['w7', 'w8', 'w9'],
+    ], [
+        ['y1', 'y2', 'y3'],
+        ['y4', 'y5', 'y6'],
+        ['y7', 'y8', 'y9'],
+    ],
+])
+
+# front=red,index 1
+# left=green,index 0
+# right=blue, index 2
+# up=white, index 4
+# down=yellow, index 5
+# back=orange, index 3
+DIRECTION = ['LA', 'LC', 'FA', 'FC', 'RA', 'RC', 'BA', 'BC', 'UA', 'UC', 'DA', 'DC']
+
+for g in END[0]:
+    print(g)
+
+new = move_left_c(END.copy())
+print()
+for n in new:
+    for a in n:
+        print(a)
+    print()
